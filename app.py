88958acc1fb17d38d150deb11d42eef5b714ad7d
yazoo1220 <podman1220@hotmail.com>
2023-08-01 00:11:42 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from streamlit_ace import st_ace
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+
+content = st_ace()
+content
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
+
+text_splitter = RecursiveCharacterTextSplitter.from_language(
+    chunk_size=1000,
+    chunk_overlap=0,
+    language=Language.PYTHON
+)
+
+if st.button('submit'):
+    texts = text_splitter.split_documents(content)
+
+def _sanitize_output(text: str):
+    try:
+        _, after = text.split("```python")
+        return after.split("```")[0]
+    except ValueError:
+        return text
+
+prompt=ChatPromptTemplate.from_template(
+    "You are a helpful assistant that add comments to each meaningful block and return the code with those cmoments. The code should be in a code block starting with ```python\n\nCODE: {input}",
+)
+chain = prompt | ChatOpenAI() | StrOutputParser() | _sanitize_output
+
+result = []
+
+for text in texts:
+    result.append(chain.invoke({"input":text.page_content}))
+
+st.markdown(''.join(result))
