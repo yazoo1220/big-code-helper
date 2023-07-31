96944816c65fe9724b5e1915f6cea527da48286c
yazoo1220 <podman1220@hotmail.com>
2023-08-01 01:04:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def _sanitize_output(text: str):
         _, after = text.split("```python")
         return after.split("```")[0]
     except ValueError:
-        return text.split("```python")[1]
+        return text.split("```python")[0]
 
 if st.button('submit'):
     with st.spinner(text='in progress...'):
